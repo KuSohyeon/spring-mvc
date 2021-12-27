aa54ac316267a14dbab52262c478100a66b5b947
구소현 <kusohyeon@gmail.com>
2021-12-27 23:29:28 +0900

    서블릿

--- src/main/java/com/example/springmvc/SpringMvcApplication.java
+++ src/main/java/com/example/springmvc/SpringMvcApplication.java
@@ -2,8 +2,10 @@ package com.example.springmvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class SpringMvcApplication {
 
     public static void main(String[] args) {
